d350986c30738e88db51eb3176b8aab9030d04aa
Mateo Miño <118315392+Mino-Mateo@users.noreply.github.com>
2023-12-25 18:32:50 -0500

    Arreglo database

--- src/database.js
+++ src/database.js
@@ -1,15 +1,13 @@
 // importar mongoose
 const mongoose = require("mongoose");
 
-const MONGODB_URI = "mongodb://localhost:27017/portfolio";
-
 // cadena de conexion a la BDD
 
 //crear un metodo para hacer la cadena de concexion
 connection = async () => {
 	try {
 		//Invocar al metodo connect
-		await mongoose.connect(MONGODB_URI);
+		await mongoose.connect(process.env.MONGODB_URI);
 		//respuesta de la promesa == "OK"
 		console.log("Database is connected");
 	} catch (error) {
